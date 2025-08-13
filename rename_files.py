84a54a83d2a9c2f0e404936f5813c9c44b96e1a2
Kenneth Delgado <kenvastolord@gmail.com>
2025-08-13 08:28:57 +0200

    Add validation for file naming
    - Verify if the file exists and it is a regular file.

--- rename_files.py
+++ rename_files.py
@@ -7,6 +7,11 @@ print("Files in the Directory: ")
 
 for file in files:
     old_file = os.path.join(directory, file)
+    if os.path.exists(old_file) and os.path.isfile(old_file):
+        print(f"the {file} exists and is a valid file")
+    else:
+        print(f"{file} is either missign or not a valid file")
+
     new_file = os.path.join(directory, "new_" + file)
     os.rename(old_file, new_file)
     print(f"rename {file} -> new_{file}")
