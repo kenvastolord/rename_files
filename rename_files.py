10c4af58f891bcd01b07d3e0a12b130fab72ead9
Kenneth Delgado <kenvastolord@gmail.com>
2025-08-14 17:52:25 +0200

    fix: normalize file extension input by adding missing dot if needed

--- rename_files.py
+++ rename_files.py
@@ -3,6 +3,9 @@ import os
 directory = "./test/"
 files = os.listdir(directory)
 
+ext_filter = input(
+    "Enter the file extensions to include (comma-separated, e.g., .jpg, .txt) or leave empty to  include all: "
+)
 
 prefix = input(
     "Enter the prefix you want to add to the file names (leave empty for no prefix): "
@@ -12,6 +15,18 @@ suffix = input(
     "Enter the suffix you want to add to the file names (leave empty for no suffix): "
 )
 
+
+extensions = []
+if ext_filter:
+    extensions = [
+        (
+            ext.strip().lower()
+            if ext.strip().startswith(".")
+            else "." + ext.strip().lower()
+        )
+        for ext in ext_filter.split(",")
+    ]
+
 for file in files:
     if file.startswith("."):
         continue
@@ -20,6 +35,9 @@ for file in files:
 
     if os.path.exists(old_file_path) and os.path.isfile(old_file_path):
         filename, ext = os.path.splitext(file)
+
+        if extensions and ext.lower() not in extensions:
+            continue
 
         new_file_name = f"{prefix + '_' if prefix else ''}{filename}{'_' + suffix if suffix else ''}{ext}"
         new_file_path = os.path.join(directory, new_file_name)
